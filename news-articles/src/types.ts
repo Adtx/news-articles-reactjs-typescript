f53c8e282f19ef4806f539b25d537131f75c73eb
Adtx <adrianodiasteixeira@gmail.com>
2022-11-19 16:23:06 +0000

    feature: Add types

---
+++ news-articles/src/types.ts
@@ -0,0 +1,19 @@
+export interface ArticleList {
+  articles: Article[]
+}
+
+export interface Article {
+  source: Source
+  author?: string
+  title: string
+  description: string
+  url: string
+  urlToImage: string
+  publishedAt: string
+  content: string
+}
+
+export interface Source {
+  id?: string
+  name: string
+}
